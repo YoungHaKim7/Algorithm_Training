4ae28384bcdb581aeef1a6ade1bc9d25de4e4284
GYoung <ytok1108@aol.com>
2024-02-24 12:07:25 +0900

    Single Linked List__ Let's go

--- 02_Algorithm_Rust_Zig_C_etc/01_Rust_Algorithm/02_data_structures/a_Singly_Linked_List_in_Rust_Ryan_Levick/singly_linked/src/lib.rs
+++ 02_Algorithm_Rust_Zig_C_etc/01_Rust_Algorithm/02_data_structures/a_Singly_Linked_List_in_Rust_Ryan_Levick/singly_linked/src/lib.rs
@@ -1,13 +1,26 @@
-enum LinkedList {
+// struct LinkedList {
+//     head: Link,
+// }
+//
+// struct Node {
+//     element: u32,
+//     next: List,
+// }
+enum Node<'a> {
     Empty,
-    NonEmpty(u32, Node)
+    NonEmpty(u32, &'a Node<'a>),
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        // let list = List::NonEmpty(1091, Box::new(Node) {
+        //     element: 1024,
+        //     next: List::Empty,
+        // });
+        let list = Node::NonEmpty(1091, &Box::new(Node::Empty));
     }
 }
