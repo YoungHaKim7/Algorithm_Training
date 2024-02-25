c2ce54516052acd964d79ff29bf6712de62300a4
GYoung <ytok1108@aol.com>
2024-02-25 13:04:36 +0900

    Copy trait?

--- 02_Algorithm_Rust_Zig_C_etc/01_Rust_Algorithm/02_data_structures/a_Singly_Linked_List_in_Rust_Ryan_Levick/singly_linked/src/lib.rs
+++ 02_Algorithm_Rust_Zig_C_etc/01_Rust_Algorithm/02_data_structures/a_Singly_Linked_List_in_Rust_Ryan_Levick/singly_linked/src/lib.rs
@@ -2,9 +2,29 @@ struct LinkedList {
     head: Link,
 }
 
+impl Copy for LinkedList {}
+
 impl LinkedList {
     fn empty() -> Self {
         Self { head: None }
+    }
+
+    fn push(&mut self, element: u32) {
+        match self.head {
+            None => {
+                self.head = Some(Box::new(Node {
+                    element,
+                    next: None,
+                }))
+            }
+            Some(n) => {
+                let new_head = Some(Box::new(Node {
+                    element,
+                    next: Some(n),
+                }));
+                self.head = new_head;
+            }
+        }
     }
 }
 
@@ -22,6 +42,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let list = LinkedList::empty();
+        let mut list = LinkedList::empty();
+        list.push(1024);
     }
 }
